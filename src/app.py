aef98c6104dc55977f317fa87c804685904dcfcf
Justin Whitaker <justinwhitaker@protonmail.com>
2022-06-07 07:23:00 -0400

    sqlalchemy

--- src/app.py
+++ src/app.py
@@ -7,7 +7,6 @@ from flask import (
     url_for,
     flash
 )
-from gpg import Data
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
@@ -17,11 +16,38 @@ from wtforms import (
     SubmitField
 )
 from wtforms.validators import DataRequired
+import os
+from flask_sqlalchemy import SQLAlchemy
+
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://{username}:{password}@{hostname}/{database}".format(
+    username = os.getenv('MYSQL_USER'),
+    password = os.getenv('MYSQL_PASSWORD'),
+    hostname = os.getenv('MYSQL_HOSTNAME'),
+    database = os.getenv('MYSQL_DATABASE'),
+)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hardcoded-secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
+
+class Widget(db.Model):
+    __tablename__ = 'widgets'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(256), unique=True, index=True)
+    value = db.Column(db.String(256))
+    reviews = db.relationship('Review', backref='role')
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(256))
+    body = db.Column(db.Text())
+    widget_id = db.Column(db.Integer, db.ForeignKey('widgets.id'))
+
+db.create_all()
 
 @app.route('/')
 def index():
@@ -31,24 +57,42 @@ def index():
     response.set_data(response_data)
     return response
 
-@app.route('/user/<string:name>')
-def user(name):
-    response = make_response()
-    response.status_code = 200
-    response_data = render_template('user.html', name=name)
-    response.set_data(response_data)
-    return response
+@app.route('/widget/<string:name>')
+def readWidget(name):
+    widget = Widget.query.filter_by(name=name).first()
+    if widget == None:
+        flash("Widget not found")
+        response = make_response()
+        response.status_code = 200
+        response_data = render_template('readWidget.html', widget=widget)
+        response.set_data(response_data)
+        return response
+    else:
+        response = make_response()
+        response.status_code = 200
+        response_data = render_template('readWidget.html', widget=widget)
+        response.set_data(response_data)
+        return response
 
-@app.route('/greeter', methods=['GET','POST'])
-def greeter():
-    form = NameForm()
+
+@app.route('/createWidget', methods=['GET','POST'])
+def createWidget():
+    form = WidgetForm()
     if form.validate_on_submit():
-        if session.get('name') != form.name.data:
-            flash('You submitted your name')
-        session['name'] = form.name.data
-        return redirect(url_for('greeter'))
-    return render_template('greeter.html', form=form, name=session.get('name'))
+        name = form.name.data
+        value = form.value.data
+        widget = Widget(name=name, value=value)
+        try:
+            db.session.add(widget)
+            db.session.commit()
+            flash("Created widget '{widget_name}'".format(widget_name=name))
+        except:
+            flash("Widget '{widget_name}' already exists".format(widget_name=name))
+        return redirect(url_for('createWidget'))
+    return render_template('createWidget.html', form=form)
 
-class NameForm(FlaskForm):
-    name = StringField("What is your name?", validators=[DataRequired()])
+class WidgetForm(FlaskForm):
+    name = StringField("Widget Name", validators=[DataRequired()])
+    value = StringField("Widget Value", validators=[DataRequired()])
     submit = SubmitField('Submit')
+
